ad9ed9eac68cfcaf7014b26bd52ec8df28e30410
Jihoon Seo <sjh7141@naver.com>
2020-09-27 20:15:51 +0900

    feat: add TransformProperty in left-bar
    - implement TransformProperty in menu no.3

--- components/left-bar-detail.vue
+++ components/left-bar-detail.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="left-detail-bar" class="d-flex ml-1 align-start justify-center">
+        <transform-property v-show="selectedmenu==3"></transform-property>
         <resize-canvas v-show="selectedmenu==5"></resize-canvas>
         <change-bg-color v-show="selectedmenu==6"></change-bg-color>
     </div>
@@ -9,6 +10,7 @@
 module.exports = {
     name: "left-bar-detail",
     components:{
+        'transform-property': 'url:./detailMenus/TransformProperty.vue',
         'change-bg-color': 'url:./detailMenus/ChangeBgColor.vue',
         'resize-canvas': 'url:./detailMenus/ResizeCanvas.vue'
     },
