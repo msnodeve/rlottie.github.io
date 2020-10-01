b351ba40d8f68e026ce8e817a4ae7c687b8aa86a
안성민 <asm9677@naver.com>
2020-10-01 15:31:52 +0900

    Fix: highlighting error fix

--- components/right-bar.vue
+++ components/right-bar.vue
@@ -121,15 +121,17 @@ module.exports = {
             this.layers = [layers];
         },
 
-        changeFocus(e){
+        changeFocus(e){            
             this.keypath = e[0]
             RLottieModule.keypath = this.isSelectAll ? (this.keypath ? this.keypath + '.**' : '**') : (this.keypath ? this.keypath : '');
-
-            RLottieModule.history.reload();
-            RLottieModule.lottieHandle.set_fill_opacity("**", 30);
-            RLottieModule.lottieHandle.set_fill_opacity(RLottieModule.keypath, 100);
-            RLottieModule.lottieHandle.set_stroke_opacity("**", 30);
-            RLottieModule.lottieHandle.set_stroke_opacity(RLottieModule.keypath, 100);            
+                        
+            if(!e[0]) {
+                RLottieModule.reload(RLottieModule.history.originJson);
+                RLottieModule.history.reload()
+                return;
+            }            
+            RLottieModule.history.highlighting((this.keypath ? this.keypath + '.**' : '**'))
+              
         },
         inputKeypath(e) {
             if(this.search) {
