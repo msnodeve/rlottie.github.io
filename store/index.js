400d4d9fd462a1adcc12191e9088b027cb2d0b2b
SungMin Ahn <asm9677@naver.com>
2020-10-14 23:51:05 +0900

    refact: change eventbus to vuex

--- store/index.js
+++ store/index.js
@@ -1,18 +1,44 @@
 var store = new Vuex.Store({
   state: {
     layers: [],
+    hasPrev: false,
+    hasNext: false,
+
+    curFrame: 0,
+    frameCount: 0,
   },
   getters: {
     getLayers(state) {
       return state.layers;
-    }
+    },
+    getCurFrame(state) {
+      return state.curFrame;
+    },
+    getFrameCount(state) {
+      return state.frameCount;
+    },
+    getHasPrev(sate) {
+      return state.hasPrev;
+    },
+    getHasNext(sate) {
+      return state.hasNext;
+    },
   },
   mutations: {
     setLayers(state, payload) {
       state.layers = payload;
-    }
+    },
+    setCurFrame(state, payload) {
+      state.curFrame = payload;
+    },
+    setFrameCount(state, payload) {
+      state.frameCount = payload;
+    },
+    setHasPrev(state, payload) {
+      state.hasPrev = payload;
+    },
+    setHasNext(state, payload) {
+      state.hasNext = payload;
+    },
   },
-  actions: {
-        
-  }
-})
+});
