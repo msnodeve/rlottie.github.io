36a81b0ccd826e1fef3cd1604811dc421ab75adb
Jihoon Seo <sjh7141@naver.com>
2020-10-16 21:41:38 +0900

    stlye: change inline css to class

--- components/upper-bar.vue
+++ components/upper-bar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="pa-0" :style="{ position: 'absolute' }">
+  <v-container fluid class="pa-0" id="upper-wrapper">
     <v-row class="pa-0">
       <v-col cols="4" offset="4" class="pa-0">
         <v-row
@@ -9,44 +9,31 @@
         >
           <v-col cols="12" class="pa-0">
             <v-expand-transition>
-              <!-- <v-row v-if="scrollBottom" class="pa-0" justify="center"> -->
               <v-col
                 v-if="scrollBottom"
                 cols="6"
                 offset="3"
-                class="pa-0"
-                style="text-align: center"
+                class="pa-0 text-center"
               >
-                <div
-                  style="
-                    width: 100%;
-                    background-color: rgba(41, 44, 49, 1);
-                    border-radius: 0px 0px 15px 15px;
-                    height: 50px;
-                    vertical-align: middle;
-                  "
-                >
+                <div id="upper-nav">
                   <v-btn
                     color="rgba( 41, 44, 49, 0.9 )"
-                    class="white--text"
+                    class="white--text height-100-percent"
                     @click="button1"
-                    style="height: 100%"
                   >
                     <v-icon dark> mdi-square-off-outline </v-icon>
                   </v-btn>
                   <v-btn
                     color="rgba( 41, 44, 49, 0.9 )"
-                    class="white--text"
+                    class="white--text height-100-percent"
                     @click="button2(0)"
-                    style="height: 100%"
                   >
                     <v-icon dark> mdi-cellphone-android </v-icon>
                   </v-btn>
                   <v-btn
                     color="rgba( 41, 44, 49, 0.9 )"
-                    class="white--text"
+                    class="white--text height-100-percent"
                     @click="button2(1)"
-                    style="height: 100%"
                   >
                     <v-icon dark> mdi-watch </v-icon>
                   </v-btn>
@@ -55,7 +42,7 @@
             </v-expand-transition>
           </v-col>
           <v-col cols="12" class="pa-0">
-            <v-col cols="4" offset="4" class="pa-0" style="text-align: center">
+            <v-col cols="4" offset="4" class="pa-0 text-center">
               <v-img
                 v-if="!scrollBottom"
                 src="./assets/upperbar_down.png"
@@ -154,5 +141,15 @@ module.exports = {
 .pretap:hover:after {
   width: 100%;
   transition: 800ms ease all;
+}
+#upper-wrapper {
+  position: absolute;
+}
+#upper-nav {
+  width: 100%;
+  background-color: rgba(41, 44, 49, 1);
+  border-radius: 0px 0px 15px 15px;
+  height: 50px;
+  vertical-align: middle;
 }
 </style>
